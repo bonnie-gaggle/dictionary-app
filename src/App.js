6a3ccf595fe1ae7d6408ac28c8e4d94ecc246939
hanksbonniec <hanksbonniec@gmail.com>
2021-04-19 20:15:02 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Dictionary</h1>
+        <p>
+          Coming soon!
+        </p>
+        <footer>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-source code
+          </a> by Bonnie Hanks
+        </footer>
+    </div>
+  );
+}
+
