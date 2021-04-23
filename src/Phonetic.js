12c2fd6cf035210a94b859e463257734f6c57576
hanksbonniec <hanksbonniec@gmail.com>
2021-04-22 20:49:13 -0700

    Added audio for pronunciation

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,12 +1,14 @@
 import React from "react";
+import "./Phonetics.css";
 
 export default function Phonetic(props){
+
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} target="_blank">
-        Hear it
-      </a><br />
       {props.phonetic.text}
+      <audio className="audio" controls src={props.phonetic.audio}> 
+        Your browser does not support the <code>audio</code> element.
+      </audio>
     </div>
   );
 }
