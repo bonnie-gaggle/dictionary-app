cca0cdcbe2801e13f2ede0d6832af4f3753b7484
hanksbonniec <hanksbonniec@gmail.com>
2021-04-22 20:19:57 -0700

    Added component for phonetics

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function SearchResults(props){
   if (props.results){
@@ -7,6 +8,13 @@ export default function SearchResults(props){
       <div className="SearchResults">
         <h2> {(props.results.word).toLowerCase()}
         </h2>
+        {props.results.phonetics.map(function(phonetic, index){
+          return(
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
         <div className="definitions">
           {props.results.meanings.map(function (meaning, index){
             return <div key={index}>
